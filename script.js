a9018f1a18daec21a8711515bb1d6db930de333d
Mojammil Husain <mojammil@gmail.com>
2025-05-07 16:53:20 +0530

    added contact me section

--- script.js
+++ script.js
@@ -39,3 +39,24 @@ bar.addEventListener('click',() =>{
     menu.classList.toggle('show_menu');
     nav.classList.toggle('nav_border');
 });
+
+// contact me form
+ 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
